4ee7be6eebe5806a5a0f82d3c4b5473dae80c974
Asif <tayyibimamasif@gmail.com>
2025-06-04 10:15:53 +0600

    add 07.js

--- 08.js
+++ 08.js
@@ -1,9 +1,9 @@
-var numbers = ["rahim", "karim", "abdul", "sadsd", "heroAlom"];
+var numbers = [1, 2, 3, 3, 4, 4, 5, 6, 7, 8, 9, 10];
 
 let maxNum = numbers[0];
 
 for (let i = 1; i < numbers.length; i++) {
-    if(maxNum.length < numbers[i].length)
+    if(maxNum < numbers[i])
     {
         maxNum = numbers[i];
     }
